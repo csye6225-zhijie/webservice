e2c1a800454922d92fc33ebcb8ae5ed54d3f8213
Zhijie-Lii <l445476530@yahoo.com>
2022-02-27 12:02:04 +0000

    refactor Controller and Service

--- src/main/java/com/csye6225/webservice/Controller/UserController.java
+++ src/main/java/com/csye6225/webservice/Controller/UserController.java
@@ -3,35 +3,22 @@ package com.csye6225.webservice.Controller;
 import com.csye6225.webservice.Model.User;
 import com.csye6225.webservice.Service.UserService;
 import com.csye6225.webservice.Model.VO.UserVO;
-import org.aspectj.bridge.MessageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
 
 @RestController
 @RequestMapping("")
 public class UserController {
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
     private UserService userService;
-    private Date date;
-    public static final String EMAIL_REGEX_DEFAULT = "^([a-z0-9A-Z]+[-|_|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-
-
-    public static boolean isValidEmail(String email) {
-        return email.matches(EMAIL_REGEX_DEFAULT);
-    }
 
     @Autowired
     public void setUserService(UserService userService){
@@ -48,115 +35,54 @@ public class UserController {
     @PostMapping(value = "/v1/user", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity newUser(@RequestBody User newUser){
-        if (!isValidEmail(newUser.getUsername()))  {
+        if (!userService.isValidEmail(newUser.getUsername()))  {
            return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
-
         //verify email uniqueness
-        User repeatedUserEmail = userService.findByUserName(newUser.getUsername());
-        if(repeatedUserEmail != null){
+        User existedUserEmail = userService.findByUserName(newUser.getUsername());
+        if(existedUserEmail != null){
             throw new DuplicateKeyException("Your email has been associated with an existing account. Please try another email address");
         }
-        User user = new User();
-        bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        date = new Date();
-        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String currentTime = sdf.format(date);
-        user.setFirst_name(newUser.getFirst_name());
-        user.setLast_name(newUser.getLast_name());
-        user.setUsername(newUser.getUsername());
-        //bcrypt
-        user.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+        if(newUser.getId() != null || newUser.getAccountCreated() != null || newUser.getAccountUpdated() != null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
 
-        user.setAccountCreated(currentTime);
-        user.setAccountUpdated(currentTime);
-
-        userService.save(user);
-        return new ResponseEntity(new UserVO(user.getId(),user.getFirst_name(),user.getLast_name(),user.getUsername(),user.getAccountCreated(),user.getAccountUpdated()),
+        UserVO userVO = userService.saveUser(newUser);
+        return new ResponseEntity(userVO,
                 HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/v1/user/self", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity get(HttpServletRequest httpRequest){
+    public ResponseEntity<?> get(HttpServletRequest httpRequest){
         final String authorization = httpRequest.getHeader("Authorization");
-        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
-            // Authorization: Basic base64credentials
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            // credentials = username:password
-            final String[] values = credentials.split(":", 2);
-            User user = userService.findByUserName(values[0]);
-            if(null == user) return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        UserVO userVO = userService.authorize(authorization);
 
-            //check and compare password
-            String rightPassword = user.getPassword();
-            if(!BCrypt.checkpw(values[1], rightPassword))
-                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if(userVO == null)
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        else {
+            return new ResponseEntity(userVO, HttpStatus.OK);
 
-            return new ResponseEntity(new UserVO(user.getId(),user.getFirst_name(),user.getLast_name(),user.getUsername(),user.getAccountCreated(),user.getAccountUpdated())
-            , HttpStatus.OK);
-        }else {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
     }
 
 
     @PutMapping (value = "/v1/user/self", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity update(HttpServletRequest httpRequest, @RequestBody User updateUser){
+    public ResponseEntity update(HttpServletRequest httpRequest, @RequestBody User updateUser) {
         final String authorization = httpRequest.getHeader("Authorization");
-        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
-            // Authorization: Basic base64credentials
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            // credentials = username:password
-            final String[] values = credentials.split(":", 2);
-            User user = userService.findByUserName(values[0]);
-            if (null == user) {
+        UserVO userVO = userService.authorize(authorization);
+        if (userVO != null) {
+
+            int updateResult = userService.updateUser(userService.findById(userVO.getId()), updateUser); //
+            if (updateResult == 0) {
                 return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            } else if (updateResult == 1) {
+                return new ResponseEntity(HttpStatus.NO_CONTENT);
             }
-
-            String rightPassword = user.getPassword();
-            if(!BCrypt.checkpw(values[1], rightPassword))
-                return new ResponseEntity(HttpStatus.UNAUTHORIZED); //
-
-            date = new Date();
-            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String currentTime = sdf.format(date);
-            //only permitted field can be modified
-            if(updateUser.getId() != null || updateUser.getAccountCreated() != null || updateUser.getAccountUpdated() != null){
-                return new ResponseEntity(HttpStatus.BAD_REQUEST); //compare if null use ==
-            }
-            if(updateUser.getUsername() != null && !updateUser.getUsername().equals(user.getUsername()) ){
-                return new ResponseEntity(HttpStatus.BAD_REQUEST); //compare string not with '=='
-            }
-
-
-            if(updateUser.getFirst_name() != null && updateUser.getFirst_name()!=""){
-                user.setFirst_name(updateUser.getFirst_name());
-            }
-            if(updateUser.getLast_name() != null && updateUser.getLast_name()!=""){
-                user.setLast_name(updateUser.getLast_name());
-            }
-            bCryptPasswordEncoder = new BCryptPasswordEncoder();
-            if(updateUser.getPassword() != null && updateUser.getPassword()!=""){
-                user.setPassword(bCryptPasswordEncoder.encode(updateUser.getPassword()));
-            }
-
-            user.setAccountUpdated(currentTime);
-
-            userService.save(user);
-            return new ResponseEntity(HttpStatus.NO_CONTENT);
-
-        }else {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
-
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
-
 
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
