9969b88703215945b4438c07a45b256c7367360f
Zhijie-Lii <l445476530@yahoo.com>
2022-03-19 12:45:29 +0000

    test-config

--- src/main/java/com/csye6225/webservice/Config/AWSConfig.java
+++ src/main/java/com/csye6225/webservice/Config/AWSConfig.java
@@ -2,28 +2,36 @@ package com.csye6225.webservice.Config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AWSConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @Bean
-    public static AmazonS3Client amazonS3Client() {
+    public AmazonS3Client amazonS3Client() {
 
         System.out.println("Starting s3 client");
 
         return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
+                .withRegion(region)
                 .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
     }
 }
 
 //@Configuration
-//public class AmazonS3Config {
+//public class AWSConfig {
 //    @Value("${cloud.aws.credentials.access-key}")
 //    private String awsAccessKey;
 //
